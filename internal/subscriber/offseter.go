11240adfd739057164179cd30721775de0b3046a
Valentin Dosimont <valentin@pupucecorp.com>
2024-03-05 10:06:10 +0100

    feat: add yielder & offseter events

--- internal/subscriber/offseter.go
+++ internal/subscriber/offseter.go
@@ -2,12 +2,15 @@ package subscriber
 
 import (
 	"github.com/carbonable/leaderboard/internal/indexer"
+	"github.com/carbonable/leaderboard/internal/leaderboard"
+	"github.com/carbonable/leaderboard/internal/starknet"
 	"github.com/charmbracelet/log"
 	"github.com/nats-io/nats.go"
+	"gorm.io/gorm"
 )
 
 // Handling offseter `Withdraw` event
-func OffseterWithdrawSubscriber(storage indexer.Storage) nats.MsgHandler {
+func OffseterWithdrawSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("offseter:withdraw", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
@@ -15,39 +18,71 @@ func OffseterWithdrawSubscriber(storage indexer.Storage) nats.MsgHandler {
 		}
 
 		log.Info("offseter:withdraw", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"value":   event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "offseter:withdraw", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 // Handling offseter `Deposit` event
-func OffseterDepositSubscriber(storage indexer.Storage) nats.MsgHandler {
+func OffseterDepositSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("offseter:deposit", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
 			return
 		}
+
 		log.Info("offseter:deposit", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"value":   event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "offseter:deposit", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 // Handling offseter `Claim` event
-func OffseterClaimSubscriber(storage indexer.Storage) nats.MsgHandler {
+func OffseterClaimSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("offseter:claim", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
 			return
 		}
+
 		log.Info("offseter:claim", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"amount":  event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "offseter:claim", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 func RegisterOffseterSubscribers(args *SubscriberArgs) error {
-	if _, err := args.nc.Subscribe("offseter:withdraw", OffseterWithdrawSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("offseter:withdraw", OffseterWithdrawSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
-	if _, err := args.nc.Subscribe("offseter:deposit", OffseterDepositSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("offseter:deposit", OffseterDepositSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
-	if _, err := args.nc.Subscribe("offseter:claim", OffseterClaimSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("offseter:claim", OffseterClaimSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
 
--- internal/subscriber/yielder.go
+++ internal/subscriber/yielder.go
@@ -2,12 +2,15 @@ package subscriber
 
 import (
 	"github.com/carbonable/leaderboard/internal/indexer"
+	"github.com/carbonable/leaderboard/internal/leaderboard"
+	"github.com/carbonable/leaderboard/internal/starknet"
 	"github.com/charmbracelet/log"
 	"github.com/nats-io/nats.go"
+	"gorm.io/gorm"
 )
 
 // Handling yielder `Withdraw` event
-func YielderWithdrawSubscriber(storage indexer.Storage) nats.MsgHandler {
+func YielderWithdrawSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("yielder:withdraw", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
@@ -15,39 +18,71 @@ func YielderWithdrawSubscriber(storage indexer.Storage) nats.MsgHandler {
 		}
 
 		log.Info("yielder:withdraw", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"value":   event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "yielder:withdraw", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 // Handling yielder `Deposit` event
-func YielderDepositSubscriber(storage indexer.Storage) nats.MsgHandler {
+func YielderDepositSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("yielder:deposit", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
 			return
 		}
+
 		log.Info("yielder:deposit", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"value":   event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "yielder:deposit", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 // Handling yiedler `Claim` event
-func YielderClaimSubscriber(storage indexer.Storage) nats.MsgHandler {
+func YielderClaimSubscriber(storage indexer.Storage, db *gorm.DB, rpc starknet.StarknetRpcClient) nats.MsgHandler {
 	return func(m *nats.Msg) {
 		event, err := decodeEvent("yielder:claim", storage.Get([]byte("EVENT#"+string(m.Data))))
 		if err != nil {
 			return
 		}
+
 		log.Info("yielder:claim", "event", event)
+
+		metadata := getMetadataFromEvent(rpc, event.FromAddress)
+		data := map[string]string{
+			"address": event.Data[0],
+			"amount":  event.Data[1],
+			"time":    event.Data[3],
+		}
+
+		evt := leaderboard.DomainEventFromStarknetEvent(event, "yielder:claim", event.Data[0], data, metadata)
+		db.Create(&evt)
 	}
 }
 
 func RegisterYielderSubscribers(args *SubscriberArgs) error {
-	if _, err := args.nc.Subscribe("yielder:withdraw", YielderWithdrawSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("yielder:withdraw", YielderWithdrawSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
-	if _, err := args.nc.Subscribe("yielder:deposit", YielderDepositSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("yielder:deposit", YielderDepositSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
-	if _, err := args.nc.Subscribe("yielder:claim", YielderClaimSubscriber(args.storage)); err != nil {
+	if _, err := args.nc.Subscribe("yielder:claim", YielderClaimSubscriber(args.storage, args.db, args.rpc)); err != nil {
 		return err
 	}
 
